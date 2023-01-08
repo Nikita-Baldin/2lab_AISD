ce689e84332c5ca90b81adbe968c565992f78e5f
Nikita-Baldin <nikitabaldin22102003@mail.ru>
2023-01-08 18:46:02 +0400

    Добавьте файлы проекта.

---
+++ 2lab_AISD/Source.cpp
@@ -0,0 +1,333 @@
+﻿#pragma once
+#include <iostream>
+template <class T>
+class Plenty {
+private:
+	T* data;
+	int size;
+	
+public:
+	Plenty() : data(nullptr), size(0) {}
+	Plenty(int size) : data(new T[size]), size(size) {}
+	Plenty(Plenty<T>& second) {
+		size = second.size;
+		data = second.data;
+	}
+	~Plenty() {
+		if (!data)
+			delete[] data;
+	}
+
+	bool check_num(T num) {
+		for (int i = 0; i < size; i++) {
+			if (num == data[i])
+				return true;
+		}
+		return false;
+	}
+
+
+	T operator[](const int i) { 
+		if (!data)
+			throw std::logic_error("arr == nullptr");
+		if (i > size - 1 || i < 0)
+			throw std::logic_error("invalide index");
+		return data[i];
+	}
+
+	Plenty& operator+=(T num) 
+	{
+		if (check_num(num) == false) {
+			Plenty tmp(size + 1);
+			tmp.size = 0;
+			for (int i = 0; i < size; i++) {
+				tmp.data[i] = data[i];
+				tmp.size++;
+			}
+			tmp.data[tmp.size] = num;
+			data = tmp.data;
+			size++;
+		}
+		return *this;
+	}
+	Plenty operator+(const Plenty& second) {
+		Plenty tmp(size + second.size);
+		tmp.size = 0;
+		for (int i = 0; i < size; i++) {
+			tmp.data[i] = data[i];
+			tmp.size++;
+		}
+		for (int i = 0; i < second.size; i++) {
+			if (check_num(second.data[i]) == false)
+			{
+				tmp.data[tmp.size] = second.data[i];
+				tmp.size++;
+			}
+		}
+		return tmp;
+	}
+	Plenty operator-(const Plenty& second) {
+		Plenty tmp(size);
+		tmp.size = 0;
+		bool flag = false;
+		for (int i = 0; i < size; i++) {
+			flag = false;
+			for (int j = 0; j < second.size; j++) {
+				if (data[i] == second.data[j])
+				{
+					flag = true;
+					break;
+				}
+			}
+			if (flag == false)
+			{
+				tmp.data[tmp.size] = data[i];
+				tmp.size++;
+			}
+		}
+		return tmp;
+	}
+	Plenty operator+(T num) {
+		Plenty tmp(size + 1);
+		tmp.size = 0;
+		if (check_num(num) == false) {
+			for (int i = 0; i < size; i++) {
+				tmp.data[i] = data[i];
+				tmp.size++;
+			}
+			tmp.data[tmp.size] = num;
+			tmp.size++;
+		}
+		return tmp;
+	}
+	Plenty operator-(T num)
+	{
+		Plenty data_n(size);
+		for (int i = 0; i < data_n.size; i++) {
+			data_n.data[i] = data[i];
+		}
+		if (check_num(num) == true) {
+			for (int i = 0; i < data_n.size; i++) {
+				if (data_n.data[i] == num)
+				{
+					for (int j = i; j < data_n.size - 1; j++) {
+						data_n.data[j] = data_n.data[j + 1];
+					}
+					data_n.size--;
+					break;
+				}
+			}
+		}
+		else std::cout << "Set have not got this num" << std::endl;
+		return data_n;
+	}
+	Plenty& operator-=(T num) {
+		if (check_num(num) == true) {
+			for (int i = 0; i < size; i++) {
+				if (data[i] == num)
+				{
+					for (int j = i; j < size - 1; j++) {
+						data[j] = data[j + 1];
+					}
+					size--;
+					break;
+				}
+			}
+		}
+		else std::cout << "Set have not got this num" << std::endl;
+		return *this;
+	}
+	Plenty intersection(const Plenty& second) {
+		Plenty tmp(size);
+		tmp.size = 0;
+		bool flag = false;
+		for (int i = 0; i < size; i++) {
+			flag = false;
+			for (int j = 0; j < second.size; j++) {
+				if (data[i] == second.data[j])
+				{
+					flag = true;
+					break;
+				}
+			}
+			if (flag == true)
+			{
+				tmp.data[tmp.size] = data[i];
+				tmp.size++;
+			}
+		}
+		return tmp;
+	}
+
+	bool operator==(const Plenty& second) { 
+		if (second.size != size)
+			return false;
+		else
+		{
+			bool flag = false;
+			for (int i = 0; i < size; i++)
+			{
+				flag = false;
+				for (int j = 0; j < second.size; j++) {
+					if (data[i] == second.data[j]) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag == false)
+					return false;
+			}
+			return true;
+		}
+	}
+
+	bool operator!=(const Plenty& second) {
+		if (second.size != size)
+			return true;
+		else
+		{
+			bool flag = true;
+			for (int i = 0; i < size; i++)
+			{
+				flag = true;
+				for (int j = 0; j < second.size; j++) {
+					if (data[i] == second.data[j]) {
+						flag = false;
+						break;
+					}
+				}
+				if (flag == true)
+					return true;
+			}
+			return false;
+		}
+	}
+	friend std::ostream& operator<<(std::ostream& s, const Plenty<T>& set)
+	{
+		for (int i = 0; i < set.size; i++) {
+			s << set.data[i] << "  ";
+		}
+		return s;
+	}
+};
+
+std::ostream& operator<<(std::ostream& out, const std::pair<int, double>& p)
+{
+	out << "{" << p.first << ", " << p.second << "}";
+	return out;
+}
+
+int main() {
+	try {
+		Plenty<int> p1, p11;
+		p1 += 6;
+		p1 += 7;
+		p1 += 8;
+		p1 += 3;
+		p1 += 4;
+		/*set11 += 2;
+		set11 += 3;
+		set11 += 7;
+		set11 += 8;*/
+		p11 = p1 + 5;
+		p11 = p1 - 6;
+		p11 -= 7;
+		std::cout << "p 1: " << p1 << std::endl;
+		std::cout << "p 11: " << p11 << std::endl;
+		std::cout << "p1+p11: " << p1 + p11 << std::endl;
+		std::cout << "p1-p11: " << p1 - p11 << std::endl;
+		std::cout << "intersection: " << p1.intersection(p11) << std::endl;
+		//std::cout << "task: " << set1.check_count(set11) << std::endl;
+		std::cout << "equality: " << (p1 == p11) << std::endl;
+		std::cout << "not equality: " << (p1 != p11) << std::endl;
+		std::cout << "index: " << p1[1] << std::endl;
+		std::cout << std::endl;
+
+		Plenty<float> p2, p22;
+		p2 += 6.4;
+		p2 += 7.5;
+		p2 += 8.9;
+		p2 += 1.1;
+		p2 += 4.9;
+		/*set22 += 3.3;
+		set22 += 6.4;
+		set22 += 7.7;*/
+		p22 = p2 + 4.4;
+		p22 = p2 - 6.4;
+		p22 -= 7.5;
+		std::cout << "p 2: " << p2 << std::endl;
+		std::cout << "p 22: " << p22 << std::endl;
+		std::cout << "p2+p22: " << p2 + p22 << std::endl;
+		std::cout << "p2-p22: " << p2 - p22 << std::endl;
+		std::cout << "intersection: " << p2.intersection(p22) << std::endl;
+		//std::cout << "task: " << set22.check_count(set2) << std::endl;
+		std::cout << "equality: " << (p2 == p22) << std::endl;
+		std::cout << "not equality: " << (p2 != p22) << std::endl;
+		std::cout << "index: " << p2[1] << std::endl;
+		std::cout << std::endl;
+
+		Plenty<std::string> p3, p33;
+		p3 += "mom";
+		p3 += "nik";
+		p3 += "kot";
+		p3 += "nen";
+		p3 += "pol";
+		/*set33 += "m";
+		set33 += "rt";
+		set33 += "w";*/
+		p33 = p3 + "ret";
+		p33 = p3 - "mom";
+		p33 -= "nik";
+		p33 += "you";
+		std::cout << "p 3: " << p3 << std::endl;
+		std::cout << "p 33: " << p33 << std::endl;
+		std::cout << "p3+p33: " << p3 + p33 << std::endl;
+		std::cout << "p3-p33: " << p3 - p33 << std::endl;
+		std::cout << "intersection: " << p3.intersection(p33) << std::endl;
+		//std::cout << "task: " << set3.check_count(set33) << std::endl;
+		std::cout << "equality: " << (p3 == p33) << std::endl;
+		std::cout << "not equality: " << (p3 != p33) << std::endl;
+		std::cout << "index: " << p3[1] << std::endl;
+		std::cout << std::endl;
+
+		Plenty<std::pair<int, double>> p4, p44;
+		p4 += {1, 3.2};
+		p4 += {2, 5.7};
+		p4 += {3, 4.4};
+		p4 += {8, 9.0};
+		p4 += {10, 3.5};
+		/*set44 += {1, 3.3};
+		set44 += {1, 3.2};
+		set44 += {2, 5.6};*/
+		p44 = p4 + std::pair<int, double>(1, 4.4);
+		p44 = p4 - std::pair<int, double>(1, 3.2);
+		p44 -= {2, 5.7};
+		std::cout << "p 4: " << p4 << std::endl;
+		std::cout << "p 44: " << p44 << std::endl;
+		std::cout << "p4+p44: " << p4 + p44 << std::endl;
+		std::cout << "p4-p44: " << p4 - p44 << std::endl;
+		std::cout << "intersection: " << p4.intersection(p44) << std::endl;
+		//std::cout << "task: " << set44.check_count(set4) << std::endl;
+		std::cout << "equality: " << (p4 == p44) << std::endl;
+		std::cout << "not equality: " << (p4 != p44) << std::endl;
+		std::cout << "index: " << p4[1] << std::endl;
+		std::cout << std::endl;
+
+		
+		Plenty<std::pair<int, double>> p, pp;
+		p += {1, 3.2};
+		p += {2, 5.7};
+		p += {3, 4.4};
+		pp = p + std::pair<int, double>(1, 4.4);
+		p += {1, 4.4};
+		std::cout << "p: " << p << std::endl;
+		std::cout << "pp: " << pp << std::endl;
+		std::cout << "equality: " << (p == pp) << std::endl;
+		std::cout << "not equality: " << (p != pp) << std::endl;
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+}
---
+++ 2lab_AISD/Source1.cpp
@@ -0,0 +1,543 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <string.h>
+#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+temlate <typename T>;
+class Plenty {
+private:
+	size_t size;
+	T* data;
+public:
+	Plenty() : data(nullptr), size(0) {}
+	Plenty(int size) : data(new size_t[size]), size(size) {}
+	Plenty(const Plenty& second);
+	~Plenty();
+
+	int operator[](const int index);
+
+	Plenty operator+(const Plenty& second);
+	Plenty operator-(const Plenty& second);
+
+	Plenty operator+(int num);
+	Plenty operator-(int num);
+
+	Plenty& operator+=(int num);
+	Plenty& operator-=(int num);
+
+	Plenty intersection(const Plenty& second);
+	Plenty ungeneral(const Plenty& second);
+
+	bool check_num(int num);
+	bool operator!=(const Plenty& second);
+
+	bool operator==(const Plenty& second);
+
+	friend std::ostream& operator<<(std::ostream& s, const Plenty& set);
+};
+
+
+Plenty::Plenty(const Plenty& second) {
+	size = second.size;
+	data = second.data;
+}
+
+Plenty:: ~Plenty() {
+	if (!data)
+		delete[] data;
+}
+int Plenty:: operator[](const int index) {
+	if (!data)
+		throw std::logic_error("arr == nullptr");
+	if (index > size - 1 || index < 0)
+		throw std::logic_error("invalide index");
+	return data[index];
+}
+Plenty Plenty:: operator+(const Plenty& second) {
+	Plenty tmp(size + second.size);
+	tmp.size = 0;
+	for (int i = 0; i < size; i++) {
+		tmp.data[i] = data[i];
+		tmp.size++;
+	}
+	for (int i = 0; i < second.size; i++) {
+		if (check_num(second.data[i]) == false)
+		{
+			tmp.data[tmp.size] = second.data[i];
+			tmp.size++;
+		}
+	}
+	return tmp;
+}
+Plenty Plenty:: operator-(const Plenty& second) {
+	Plenty tmp(size);
+	tmp.size = 0;
+	bool flag = false;
+	for (int i = 0; i < size; i++) {
+		flag = false;
+		for (int j = 0; j < second.size; j++) {
+			if (data[i] == second.data[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (flag == false)
+		{
+			tmp.data[tmp.size] = data[i];
+			tmp.size++;
+		}
+	}
+	return tmp;
+}
+Plenty Plenty:: operator+(int num) {
+	if (!data)
+	{
+		Plenty tmp(size);
+		tmp.data[0] = num;
+		tmp.size++;
+		return tmp;
+	}
+	else {
+		if (check_num(num) == false)
+		{
+			Plenty tmp(size);
+			for (int i = 0; i < tmp.size; i++) {
+				tmp.data[i] = data[i];
+			}
+			tmp.data[tmp.size] = num;
+			tmp.size++;
+			return tmp;
+		}
+	}
+}
+Plenty Plenty:: operator-(int num)
+{
+	Plenty data_n(size);
+	for (int i = 0; i < data_n.size; i++) {
+		data_n.data[i] = data[i];
+	}
+	if (check_num(num) == true) {
+		for (int i = 0; i < data_n.size; i++) {
+			if (data_n.data[i] == num)
+			{
+				for (int j = i; j < data_n.size - 1; j++) {
+					data_n.data[j] = data_n.data[j + 1];
+				}
+				data_n.size--;
+				break;
+			}
+		}
+	}
+	else std::cout << "Set have not got this num" << std::endl;
+	return data_n;
+}
+
+Plenty& Plenty::operator+=(int num)
+{
+	if (!data)
+	{
+		Plenty tmp(size);
+		tmp.data[0] = num;
+		data = tmp.data;
+		size++;
+
+	}
+	if (check_num(num) == false) {
+		Plenty tmp(size);
+		for (int i = 0; i < size; i++) {
+			tmp.data[i] = data[i];
+		}
+		tmp.data[tmp.size] = num;
+		data = tmp.data;
+		size++;
+	}
+	return *this;
+}
+
+Plenty& Plenty:: operator-=(int num) {
+	if (check_num(num) == false)
+		throw std::logic_error("Set have not got this num");
+	for (int i = 0; i < size; i++) {
+		if (data[i] == num)
+		{
+			for (int j = i; j < size - 1; j++) {
+				data[j] = data[j + 1];
+			}
+			size--;
+			break;
+		}
+	}
+	return *this;
+}
+Plenty Plenty::intersection(const Plenty& second) {
+	Plenty tmp(size);
+	tmp.size = 0;
+	bool flag = false;
+	for (int i = 0; i < size; i++) {
+		flag = false;
+		for (int j = 0; j < second.size; j++) {
+			if (data[i] == second.data[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (flag == true)
+		{
+			tmp.data[tmp.size] = data[i];
+			tmp.size++;
+		}
+	}
+	return tmp;
+}
+bool Plenty::check_num(int num) {
+	if (!data)
+		throw std::logic_error("data == nullptr in check_num");
+	for (int i = 0; i < size; i++) {
+		if (num == data[i])
+			return true;
+	}
+	return false;
+}
+bool Plenty:: operator==(const Plenty& second) {
+	if (second.size != size)
+		return false;
+	else
+	{
+		bool flag = false;
+		for (int i = 0; i < size; i++)
+		{
+			flag = false;
+			for (int j = 0; j < second.size; j++) {
+				if (data[i] == second.data[j]) {
+					flag = true;
+					break;
+				}
+			}
+			if (flag == false)
+				return false;
+		}
+		return true;
+	}
+}
+
+bool Plenty:: operator!=(const Plenty& second) {
+	if (second.size != size)
+		return true;
+	else
+	{
+		bool flag = true;
+		for (int i = 0; i < size; i++)
+		{
+			flag = true;
+			for (int j = 0; j < second.size; j++) {
+				if (data[i] == second.data[j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true)
+				return true;
+		}
+		return false;
+	}
+}
+std::ostream& operator<<(std::ostream& s, const Plenty& plenty) {
+	for (int i = 0; i < plenty.size; i++) {
+		s << plenty.data[i] << " ";
+	}
+	return s;
+}
+
+void menu() {
+	cout << "1.Add elements" << endl;
+	cout << "2.Delete elements" << endl;
+	cout << "3.Sum plenty" << endl;
+	cout << "4.Div plenty" << endl;
+	cout << "5.Element by index" << endl;
+	cout << "6.Intersection" << endl;
+	cout << "7.Find num in plenty" << endl;
+	cout << "8.Ungeneral" << endl;
+	cout << "9.Compare" << endl;
+	cout << "0.Add in plenty plenty with num" << endl;
+}
+void menu_2() {
+	cout << "1.plenty" << endl;
+	cout << "2.plenty" << endl;
+}
+void menu_3() {
+	cout << "1.p1-p2" << endl;
+	cout << "2.p2-p1" << endl;
+}
+void menu_4() {
+	cout << "1.p1 with i" << endl;
+	cout << "2.p2 with i" << endl;
+}
+
+int main() {
+	try {
+		Plenty p1;
+		Plenty p2;
+		Plenty p3;
+
+		while (true)
+		{
+			system("cls");
+			menu();
+			size_t flag;
+			cin >> flag;
+			switch (flag)
+			{
+			case 1:
+			{
+				bool fg = true;
+				while (fg) {
+					system("cls");
+					menu_2();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p1 += i;
+						cout << "p1= " << p1 << endl;
+						system("pause");
+						fg = false;
+						break;
+					}
+					case 2:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p2 += i;
+						cout << "p2= " << p2 << endl;
+						system("pause");
+						fg = false;
+						break;
+					}
+					}
+				} break;
+			}
+			case 2:
+			{
+				bool fg2 = true;
+				while (fg2) {
+					system("cls");
+					menu_2();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p1 -= i;
+						cout << "p1= " << p1 << endl;
+						system("pause");
+						fg2 = false;
+						break;
+					}
+					case 2:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p2 - i;
+						cout << "p2= " << p2 << endl;
+						system("pause");
+						fg2 = false;
+						break;
+					}
+					}
+
+				} break;
+			}
+			case 3:
+			{
+				cout << "p3= " << p1 + p2 << endl;
+				system("pause");
+				break;
+			}
+			case 4:
+			{
+				bool fg3 = true;
+				while (fg3) {
+					system("cls");
+					menu_3();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						cout << "p3= " << p1 - p2 << endl;
+						system("pause");
+						fg3 = false;
+						break;
+					}
+					case 2:
+					{
+						cout << "p3= " << p2 - p1 << endl;
+						system("pause");
+						fg3 = false;
+						break;
+					}
+					}
+
+				} break;
+			}
+			case 5:
+			{
+				bool fg4 = true;
+				while (fg4) {
+					system("cls");
+					menu_2();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						cout << "p1[i]= " << p1[i] << endl;
+						system("pause");
+						fg4 = false;
+						break;
+					}
+					case 2:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						cout << "p2= " << p2[i] << endl;
+						system("pause");
+						fg4 = false;
+						break;
+					}
+					}
+
+				} break;
+			}
+			case 6:
+			{
+				cout << "intersection : " << p1.intersection(p2) << endl;
+				system("pause");
+				break;
+			}
+			case 7:
+			{
+				bool fg7 = true;
+				while (fg7) {
+					system("cls");
+					menu_2();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						bool p = p1.check_num(i);
+						if (p == true) {
+							cout << "plenty has num" << endl;
+						}
+						else {
+							cout << "plenty has not num" << endl;
+						}
+						system("pause");
+						fg7 = false;
+						break;
+					}
+					case 2:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						bool p = p2.check_num(i);
+						if (p == true) {
+							cout << "plenty has num" << endl;
+						}
+						else {
+							cout << "plenty has not num" << endl;
+						}
+						system("pause");
+						fg7 = false;
+						break;
+					}
+					}
+
+				} break;
+			}
+			case 8:
+			{
+				cout << "ungeneral: " << (p1 - p2) + (p2 - p1) << endl;
+				system("pause");
+				break;
+			}
+			case 9:
+			{
+				bool p = p1 != p2;
+				cout << "Result : " << p << endl;
+				system("pause");
+				break;
+			}
+			case 0:
+			{
+				bool fg8 = true;
+				while (fg8) {
+					system("cls");
+					menu_4();
+					size_t flag_2;
+					cin >> flag_2;
+					switch (flag_2)
+					{
+					case 1:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p3 = p1 + i;
+						cout << "p3 :" << p3 << endl;
+						system("pause");
+						fg8 = false;
+						break;
+					}
+					case 2:
+					{
+						int i;
+						cout << "i=";
+						cin >> i;
+						p3 = p2 + i;
+						cout << "p3 :" << p3 << endl;
+						system("pause");
+						fg8 = false;
+						break;
+					}
+					}
+				}
+
+			}
+			}
+		}
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+}
