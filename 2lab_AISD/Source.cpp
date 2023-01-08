d83033fc0d22c797a8e09c91b0567800a8e8a503
Nikita-Baldin <nikitabaldin22102003@mail.ru>
2023-01-08 23:37:32 +0400

    fdfbdfb

--- 2lab_AISD/Source.cpp
+++ 2lab_AISD/Source.cpp
@@ -225,10 +225,6 @@ int main() {
 		p1 += 8;
 		p1 += 3;
 		p1 += 4;
-		/*set11 += 2;
-		set11 += 3;
-		set11 += 7;
-		set11 += 8;*/
 		p11 = p1 + 5;
 		p11 = p1 - 6;
 		p11 -= 7;
@@ -237,7 +233,6 @@ int main() {
 		std::cout << "p1+p11: " << p1 + p11 << std::endl;
 		std::cout << "p1-p11: " << p1 - p11 << std::endl;
 		std::cout << "intersection: " << p1.intersection(p11) << std::endl;
-		//std::cout << "task: " << set1.check_count(set11) << std::endl;
 		std::cout << "equality: " << (p1 == p11) << std::endl;
 		std::cout << "not equality: " << (p1 != p11) << std::endl;
 		std::cout << "index: " << p1[1] << std::endl;
@@ -249,9 +244,6 @@ int main() {
 		p2 += 8.9;
 		p2 += 1.1;
 		p2 += 4.9;
-		/*set22 += 3.3;
-		set22 += 6.4;
-		set22 += 7.7;*/
 		p22 = p2 + 4.4;
 		p22 = p2 - 6.4;
 		p22 -= 7.5;
@@ -260,7 +252,6 @@ int main() {
 		std::cout << "p2+p22: " << p2 + p22 << std::endl;
 		std::cout << "p2-p22: " << p2 - p22 << std::endl;
 		std::cout << "intersection: " << p2.intersection(p22) << std::endl;
-		//std::cout << "task: " << set22.check_count(set2) << std::endl;
 		std::cout << "equality: " << (p2 == p22) << std::endl;
 		std::cout << "not equality: " << (p2 != p22) << std::endl;
 		std::cout << "index: " << p2[1] << std::endl;
@@ -272,9 +263,6 @@ int main() {
 		p3 += "kot";
 		p3 += "nen";
 		p3 += "pol";
-		/*set33 += "m";
-		set33 += "rt";
-		set33 += "w";*/
 		p33 = p3 + "ret";
 		p33 = p3 - "mom";
 		p33 -= "nik";
@@ -284,7 +272,6 @@ int main() {
 		std::cout << "p3+p33: " << p3 + p33 << std::endl;
 		std::cout << "p3-p33: " << p3 - p33 << std::endl;
 		std::cout << "intersection: " << p3.intersection(p33) << std::endl;
-		//std::cout << "task: " << set3.check_count(set33) << std::endl;
 		std::cout << "equality: " << (p3 == p33) << std::endl;
 		std::cout << "not equality: " << (p3 != p33) << std::endl;
 		std::cout << "index: " << p3[1] << std::endl;
@@ -296,9 +283,6 @@ int main() {
 		p4 += {3, 4.4};
 		p4 += {8, 9.0};
 		p4 += {10, 3.5};
-		/*set44 += {1, 3.3};
-		set44 += {1, 3.2};
-		set44 += {2, 5.6};*/
 		p44 = p4 + std::pair<int, double>(1, 4.4);
 		p44 = p4 - std::pair<int, double>(1, 3.2);
 		p44 -= {2, 5.7};
@@ -307,7 +291,6 @@ int main() {
 		std::cout << "p4+p44: " << p4 + p44 << std::endl;
 		std::cout << "p4-p44: " << p4 - p44 << std::endl;
 		std::cout << "intersection: " << p4.intersection(p44) << std::endl;
-		//std::cout << "task: " << set44.check_count(set4) << std::endl;
 		std::cout << "equality: " << (p4 == p44) << std::endl;
 		std::cout << "not equality: " << (p4 != p44) << std::endl;
 		std::cout << "index: " << p4[1] << std::endl;
